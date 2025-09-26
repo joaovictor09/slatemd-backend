ec5f7333a3e6ece9916ebd018096c6554d6d35b6
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-09-26 11:41:50 -0300

    refactor: remove unused imports in UserController and CreateUserDTO

--- src/main/java/com/joaovictor09/slatemd/modules/user/dtos/CreateUserDTO.java
+++ src/main/java/com/joaovictor09/slatemd/modules/user/dtos/CreateUserDTO.java
@@ -2,7 +2,6 @@ package com.joaovictor09.slatemd.modules.user.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
